65dad296b6f561b9ec79068d1558f9b3e3ce798b
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-03-13 01:21:46 +0000

    create simulatoir

--- simulador/src/App.jsx
+++ simulador/src/App.jsx
@@ -1,15 +1,23 @@
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import Header from './components/Header'
 import { fetchMeses, fetchNav, fetchProductos, fetchUsuarioInicial } from './Services/api';
 import { LoadingSpinner } from './components/LoadingSpinner';
 import Navigation from './components/Navigation';
+import SimulacionScreen from './Screen/SimulacionScreen';
 
 function App() {
-  const [activeNav, setActiveNav] = useState('');
-  const [nav, setNav] = useState([]);
-  const [productos, setProductos] = useState([]);
-  const [meses, setMeses] = useState([]);
   const [userData, setUserData] = useState({});
+  const [nav, setNav] = useState([]);
+  const [activeNav, setActiveNav] = useState('');
+  const [productos, setProductos] = useState([]);
+  const [productoSelec, setProductoSelec] = useState('');
+  const [meses, setMeses] = useState([]);
+  const [metaGanancia, setMetaGanancia] = useState(900000)
+  const [nivelAct, setNivelAct] = useState(15)
+  const [presentaciones, setPresentaciones] = useState(5)
+  const [tasaDeCierre, setTasaDeCierre] = useState(0.3)
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -23,6 +31,7 @@ function App() {
         ])
         setNav(navData); 
         setProductos(productosData);
+        setProductoSelec(productosData[4].nombre);
         setMeses(mesesData);
         setUserData(userData);
         if (navData.length > 0) {
@@ -42,10 +51,36 @@ function App() {
     return <LoadingSpinner/>;
   }
 
+  const handlerUserData = (campo, valor) =>{
+    setUserData({...userData, [campo]: valor})
+  };
+
   return (
     <div className='flex flex-col bg-gray-300 w-full h-screen'>
       <Header />
       <Navigation activeNav={activeNav} setActiveNav={setActiveNav} nav={nav} />
+      <main className='flex-1 p-4'>
+      <Suspense fallback={<LoadingSpinner />}>
+        {activeNav === "simulator" && <SimulacionScreen 
+        userData={userData}
+        handlerUserData={handlerUserData}
+        productoActual={productoActual}
+        nivelActual={nivelActual}
+        setNivelActual={setNivelActual}
+        metaGanancia={metaGanancia}
+        setMetaGanancia={setMetaGanancia}
+        ventasActuales={ventasActuales}
+        setVentasActuales={setVentasActuales}
+        completionPercentage={completionPercentage}
+        percentDisplay={percentDisplay}
+        comisionActual={comisionActual}
+        tengoQueVender={tengoQueVender}
+        volumenCarrera={volumenCarrera}
+        totalVentasMes={totalVentasMes}
+        meses={meses}
+        />}
+      </Suspense>
+      </main>
     </div>
   )
 }
--- simulador/src/Screen/SimulacionScreen.jsx
+++ simulador/src/Screen/SimulacionScreen.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Section from '../components/Section.jsx'
+import { Users } from 'lucide-react';
 
 export default function SimulacionScreen() {
   return (
-    <div>SimulacionScreen</div>
+    <div className="space-y-4 pb-6">
+      <Section title="Variables de Usuario" icon={<Users size={20}/>}>
+      <div>{u}</div>
+      </Section>
+    </div>
   )
 }
